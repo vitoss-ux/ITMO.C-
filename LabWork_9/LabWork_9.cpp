76397b4cb87088488cffd93c98a45cc076129c85
vitoss <mvm90@yandex.ru>
2021-08-10 16:05:06 +0300

    Add lab 9

---
+++ LabWork_9/LabWork_9.cpp
@@ -0,0 +1,28 @@
+﻿// LabWork_9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <math.h>
+#include "Triangle.cpp"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	int a, b, c;
+	cout << "Введите стороны треугольника: " << endl;
+	cin >> a >> b >> c;
+	try {
+		Triangle tr(a, b, c);
+		tr.Square();
+
+	}
+	catch(SideError& ex){
+		ex.printMessage();
+		return 1;
+	}
+}
---
+++ LabWork_9/Triangle.cpp
@@ -0,0 +1,35 @@
+#include "Triangle.h"
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+class SideError
+{
+public:
+	SideError() : message("Œÿ»¡ ¿!!!") {}
+	void printMessage() const { cout << message << endl; }
+private:
+	string message;
+};
+
+class Triangle
+{
+public:
+
+	Triangle(int aSide, int bSide, int cSide) {
+		a = aSide;
+		b = bSide;
+		c = cSide;
+		if ((a + b < c) && (b + c < a) && (a + c < b))
+			throw SideError();
+	}
+
+	void Square() {
+		int p = (a + b + c) / 2;
+		cout << sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+
+private:
+	int a, b, c;
+};
