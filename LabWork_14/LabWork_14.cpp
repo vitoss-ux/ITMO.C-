62bd28dfcdb8236765afefec728acebbc0084133
vitoss <mvm90@yandex.ru>
2021-08-23 22:39:22 +0300

    Add 14, 15

---
+++ LabWork_14/LabWork_14.cpp
@@ -0,0 +1,39 @@
+﻿// LabWork_14.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+
+template <class Class>
+
+Class avrg(Class arr[], int size)
+{
+	Class sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+	return sum / size;
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	int mas[] = { 56, 967, 43, 4, 4, 90, 31, 111, 112 };
+	double mas_d[] = { 3.14, 25.234, 3.045, 7.6, 3.43, 0.34 };
+	long mas_l[] = { 98585064, 4164778, 124330785, 786676999, 11111111, 909099909976 };
+	char mas_c[] = { 'H', 'e', 'l', 'l', 'o', 'A', 'l', 'o', 'h', 'a' };
+	int i = sizeof(mas) / sizeof(mas[0]);
+	int d = sizeof(mas_d) / sizeof(mas_d[0]);
+	int l = sizeof(mas_l) / sizeof(mas_l[0]);
+	int c = sizeof(mas_c) / sizeof(mas_c[0]);
+
+	cout << "Среднее значение типа - int: " << avrg(mas, i) << endl;
+	cout << "Среднее значение типа - double: " << avrg(mas_d, d) << endl;
+	cout << "Среднее значение типа - long: " << avrg(mas_l, l) << endl;
+	cout << "Среднее значение - char: " << int(avrg(mas_c, c)) << " " << avrg(mas_c, c) << endl;
+}
---
+++ LabWork_15/LabWork_15.cpp
@@ -0,0 +1,68 @@
+﻿// LabWork_15.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <map>
+
+using namespace std;
+
+struct Grade
+{
+	string name;
+	char value;
+};
+
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	Grade student;
+	map<string, char> studentsMap;
+	map<string, char>::iterator it;
+
+	int n;
+	cout << "Введите количество студентов в группе: " << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Имя студента: ";
+		cin >> student.name;
+		student.value = 0;
+		studentsMap.insert(make_pair(student.name, student.value));
+	}
+
+	string searchName;
+	char input;
+	do
+	{
+		cout << "Имя студента для присвоения оценки: ";
+		cin >> searchName;
+
+		it = studentsMap.find(searchName);
+		if (it == studentsMap.end()) {
+			cout << "Студент не найден." << endl;
+		}
+		else
+		{
+			char grade;
+			cout << "Оценка: ";
+			cin >> grade;
+			it->second = grade;
+		}
+
+		cout << endl;
+		cout << "Продолжить? (д / н) ";
+		cin >> input;
+		if (input == 'д')
+			continue;
+	} while (input != 'н');
+
+	it = studentsMap.begin();
+	for (int i = 0; it != studentsMap.end(); it++, i++) {
+		cout << "Имя: " << it->first << ", Оценка: " << it->second << endl;
+	}
+}
